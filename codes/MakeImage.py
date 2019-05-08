27f964eedb9f7fc596ba6862656f4e1f4ca9a1c9
Minwoo Jeong <jmw95217@gmail.com>
2019-05-08 16:23:03 +0900

    rewrite the script to make images

--- codes/MakeImage.py
+++ codes/MakeImage.py
@@ -1,216 +1,269 @@
 #!/extdata6/Doyeon/anaconda3/envs/deep-purity/bin/python3.6
+"""
+Make images for training or testing our CNN model
 
+* Arguments of this script
+    1. out_image_path = a path of a output image (that will be stored using pickle)
+    2. var_tsv_file = a path of a file that contains contig, pos, ref, alt, vaf (this is a summary of MTO file)
+    3. tumor_bam_path = a path of a tumor bam file
+    4. normal_bam_path = a path of a normal bam file
+    5. tumor purity (optional; only used for training and testing): a ratio of tumor samples
+"""
 import sys
 import pysam
-import numpy
+import numpy as np
 import pickle
 import time
 
-class MakeCNNImage(object):
+from decimal import Decimal
 
+
+class ImageMaker(object):
+    """
+    a class for making images for our CNN
+    """
     def __init__(self):
+        """
+        This initialization is called to make an image.
+        """
+        self.out_image_path = sys.argv[1]
+        self.var_tsv_path = sys.argv[2]
+        self.tumor_bam_path = sys.argv[3]
+        self.norm_bam_path = sys.argv[4]
 
-        self.pos_ = sys.argv[1]
-        self.true_ratio = float(sys.argv[2])
-        self.tumor_ = sys.argv[3]
-        self.normal_ = sys.argv[4]
-        self.out_pkl_ = sys.argv[5]
-        self.width = int(sys.argv[6])
-        self.hist_height = int(sys.argv[7])
+        # parameters for images
+        self.allele_image_height = 50
+        self.allele_image_width = 1000
+        self.allele_image_channel_cnt = 9
 
-        list_pos = self.load_pos_file(self.pos_)
-        dic_array = self.fetch_image_arrays(list_pos, self.tumor_, self.normal_)
+        self.hist_height = 100
+        self.hist_width = 101
+        self.hist_channel_cnt = 1
 
-        if dic_array['read_image'].shape != (50, self.width, 9):
-            print(dic_array['read_image'].shape)
-            sys.exit()
-        if dic_array['vaf_hist_image'].shape != (self.hist_height, 101, 1):
-            print(dic_array['vaf_hist_image'].shape)
-            sys.exit()
-        dic_array['true_ratio'] = self.true_ratio
-        self.dump_array(dic_array, self.out_pkl_)
+        variants = self.load_variants()
+        var_allele_image, vaf_hist_image = self.fetch_images(variants)
+        tumor_purity = float(sys.argv[5]) if len(sys.argv) >= 5 else None  # used for training
 
-    def dump_array(self, dic_array, out_pkl_):
+        self.save_learn_info(var_allele_image, vaf_hist_image, tumor_purity)
 
-        out_pkl = open(out_pkl_, 'wb')
-        pickle.dump(dic_array, out_pkl, protocol=pickle.HIGHEST_PROTOCOL)
-        out_pkl.close()
+    def load_variants(self):
+        """
+        :return: a list of '_Variant' objects with top 1000 LODt scores from {self.var_tsv_path} file
+        """
+        print('[LOG] Load variants')
+        variants = _Variant.parse_tsv_file(self.var_tsv_path)
 
-        return
+        if len(variants) < self.allele_image_width:  # over-sampling
+            variants = list(np.random.choice(variants, self.allele_image_width))
 
-    def load_pos_file(self, pos_):
+        variants.sort(key=lambda variant: variant.lodt, reverse=True)
+        variants = variants[:self.allele_image_width]
 
-        list_pos = []
-        pos_file = open(pos_, 'r')
-        for line in pos_file:
-            cols =  line.strip('\n').split('\t')
-            contig = cols[0]
-            zero_based_pos = int(cols[1])-1
-            ref = cols[2]
-            alt = cols[3]
-            vaf = float(cols[5])
-            list_pos.append((contig, zero_based_pos, ref, alt, vaf))
-        pos_file.close()
+        return variants
 
-        return list_pos
+    def fetch_images(self, variants):
+        """
+        Make an allele image of all variants and a VAF histogram image,
+        and return both images (variant allele image, histogram image)
+        """
+        print('[LOG] Make an allele image for all variant positions', file=sys.stderr)
+        var_allele_image = np.empty((self.allele_image_height, 0, self.allele_image_channel_cnt))
+        vaf_list = []
 
-    def fetch_raw_reads_from_bam(self, tumor_, contig, zero_based_pos):
+        for variant in variants:
+            vaf = variant.vaf
+            vaf_list.append(vaf)
 
-        list_raw_read = []
-        tumor_data = pysam.AlignmentFile(tumor_, 'rb')
-        for pileupcolumn in tumor_data.pileup(contig, zero_based_pos, zero_based_pos+1, truncate=True, stepper='all'): ## filter pcr dup, qc fail, ...
-            for pileupread in pileupcolumn.pileups:
-                list_raw_read.append(pileupread)
-        tumor_data.close()
+            tumor_alleles = self._get_alleles(self.tumor_bam_path, variant)
+            normal_alleles = self._get_alleles(self.norm_bam_path, variant)
 
-        return list_raw_read
+            var_allele_one_hot = ImageMaker._one_hot_encode(tumor_alleles, normal_alleles, variant)
+            var_allele_one_hot = np.asarray(var_allele_one_hot).T
+            var_allele_one_hot = var_allele_one_hot.reshape(self.allele_image_height, 1, self.allele_image_channel_cnt)
+            var_allele_image = np.append(var_allele_image, var_allele_one_hot, axis=1)
 
-    def fetch_image_arrays(self, list_pos, tumor_, normal_):
+        print('[LOG] Make a VAF histogram image')
+        vaf_hist = dict()
+        vaf_hist_interval = Decimal(1) / (self.hist_width - 1)
 
-        list_array = []
-        list_vaf = []
-        for pos in list_pos:
-            contig = pos[0]
-            zero_based_pos = pos[1]
-            vaf = pos[4]
-            list_vaf.append(vaf)
+        for x in range(self.hist_width):
+            vaf_hist[float(vaf_hist_interval * x)] = 0
 
-            list_tumor_read = self.fetch_raw_reads_from_bam(tumor_, contig, zero_based_pos)
-            list_tumor_read = [ x for x in list_tumor_read if x.query_position != None ]
+        for vaf in vaf_list:
+            vaf_hist[round(vaf, 2)] += 1
 
-            if len(list_tumor_read) > 0 and len(list_tumor_read) < 50:
-                list_tumor_read = numpy.random.choice(list_tumor_read, size=50)
-            elif len(list_tumor_read) > 50:
-                list_tumor_read = numpy.random.choice(list_tumor_read, size=50, replace=False)
+        vaf_hist_image = []  # a 2D list that represents the VAF histogram
 
-            list_tumor_read = sorted(list_tumor_read, key=lambda x:x.alignment.mapping_quality, reverse=True)
+        for vaf in sorted(vaf_hist.keys()):
+            var_cnt = vaf_hist[vaf]
+            vaf_col = [1.0] * self.hist_height
+            zero_cnt = max(0, self.hist_height - var_cnt)
 
-            list_tumor_base = []
-            for pileupread in list_tumor_read:
-                query_pos = pileupread.query_position
-                if query_pos == None:
-                    list_tumor_base.append('D')
-                    continue
+            for i in range(zero_cnt):
+                vaf_col[i] = 0.0
 
-                query_base = pileupread.alignment.query_sequence[query_pos].upper()
-                list_tumor_base.append(query_base)
+            vaf_hist_image.append(vaf_col)
 
-            dic_for_sorting = {pos[2]:0, pos[3]:2, 'N':1}
-            for nt in ['A','C','G','T']:
-                if nt != pos[2] and nt != pos[3]:
-                    dic_for_sorting[nt] = 1
+        vaf_hist_image = np.asarray(vaf_hist_image).T
+        vaf_hist_image = vaf_hist_image[:, :, np.newaxis]
 
-            list_tumor_base = sorted(list_tumor_base, key=lambda x:dic_for_sorting[x], reverse=True)
+        return var_allele_image, vaf_hist_image
 
-            list_normal_read = self.fetch_raw_reads_from_bam(normal_, contig, zero_based_pos)
-            list_normal_read = [ x for x in list_normal_read if x.query_position != None ]
+    def save_learn_info(self, var_allele_image, vaf_hist_image, tumor_purity=None):
+        """
+        Save a dictionary that contains information used for training or testing our learning model
+        """
+        print('[LOG] Save information for learning')
 
-            if len(list_normal_read) > 0 and len(list_normal_read) < 50:
-                list_normal_read = numpy.random.choice(list_normal_read, size=50)
-            elif len(list_normal_read) > 50:
-                list_normal_read = numpy.random.choice(list_normal_read, size=50, replace=False)
+        learn_info_dict = {
+            'var_allele_image': var_allele_image,
+            'vaf_hist_image': vaf_hist_image,
+            'tumor_purity': tumor_purity  # for training
+        }
 
-            list_normal_read = sorted(list_normal_read, key=lambda x:x.alignment.mapping_quality, reverse=True)
+        with open(self.out_image_path, 'wb') as out_image_file:
+            pickle.dump(learn_info_dict, out_image_file, protocol=pickle.HIGHEST_PROTOCOL)
 
-            list_normal_base = []
-            for pileupread in list_normal_read:
-                query_pos = pileupread.query_position
-                if query_pos == None:
-                    list_normal_base.append('D')
-                    continue
+    def _get_alleles(self, bam_path, variant):
+        """
+        :return: a list of allele bases of a variant position
+        """
+        # get reads overlapped with the variant
+        reads = ImageMaker._fetch_reads(bam_path, variant.chrom, variant.pos - 1)
+        reads = list(filter(lambda x: x.query_position is not None, reads))
 
-                query_base = pileupread.alignment.query_sequence[query_pos].upper()
-                list_normal_base.append(query_base)
+        if len(reads) < self.allele_image_height:
+            reads = np.random.choice(reads, size=self.allele_image_height).tolist()
+        else:
+            reads = np.random.choice(reads, size=self.allele_image_height, replace=False).tolist()
 
-            list_normal_base = sorted(list_normal_base, key=lambda x:dic_for_sorting[x], reverse=True)
+        reads.sort(key=lambda x: x.alignment.mapping_quality, reverse=True)
 
-            if len(list_tumor_base) == 0:
-                list_tumor_base = [ 'X' for x in range(50) ]
-            if len(list_normal_base) == 0:
-                list_normal_base = [ 'X' for x in range(50) ]
+        base_sort_prior_dict = dict()
 
-            list_one_hot_encoding_array = []
-            nts = ('A', 'C', 'G', 'T')
-            for nt in nts:
-                list_nt = []
-                #for base in list_base:
-                for idx in range(50):
-                    base = list_tumor_base[idx]
-                    if base == nt:
-                        list_nt.append(1.0)
-                    else:
-                        list_nt.append(0.0)
-                list_one_hot_encoding_array.append(list_nt)
+        for base in ['A', 'C', 'G', 'T', 'N']:
+            base_sort_prior_dict[base] = 1
 
-            list_ref_alt = []
-            for base in list_tumor_base:
-                if base == pos[2]:
-                    list_ref_alt.append(0.0)
-                elif base == pos[3]:
-                    list_ref_alt.append(1.0)
+        base_sort_prior_dict[variant.ref] = 2
+        base_sort_prior_dict[variant.alt] = 0
+
+        # get query bases of all reads
+        alleles = []
+
+        for pileup_read in reads:
+            query_pos = pileup_read.query_position
+            query_base = pileup_read.alignment.query_sequence[query_pos].upper()
+            alleles.append(query_base)
+
+        alleles.sort(key=lambda x: base_sort_prior_dict[x])
+        return alleles
+
+    @staticmethod
+    def _fetch_reads(bam_path, contig, zero_based_pos):
+        """
+        Fetch reads with given a contig and position from a bam file
+        :return:  a list of 'PileupRead' objects
+        """
+        raw_reads = []
+
+        with pysam.AlignmentFile(bam_path, 'rb') as bam_file:
+            # remove PCR duplicates and QC fail reads
+            pileup_cols = bam_file.pileup(contig, zero_based_pos, zero_based_pos + 1, truncate=True, stepper='all')
+
+            for pileup_column in pileup_cols:
+                for pileup_read in pileup_column.pileups:
+                    raw_reads.append(pileup_read)
+
+        return raw_reads
+
+    @staticmethod
+    def _one_hot_encode(tumor_alleles, normal_alleles, variant):
+        """
+        :return: a 2D list that represents information of alleles (9 * self.allele_image_height)
+        """
+        # row: tumor_A, tumor_C, tumor_G, tumor_T, alt_allele, normal_A, normal_C, normal_G, normal_T
+        # column: an one-hot encode of each base
+        encode_result = []
+        nts = ('A', 'C', 'G', 'T')
+
+        # make rows for types of tumor alleles
+        for nt in nts:
+            tumor_base_binaries = []
+
+            for base in tumor_alleles:
+                if base == nt:
+                    tumor_base_binaries.append(1.0)
                 else:
-                    list_ref_alt.append(0.5)
-            list_one_hot_encoding_array.append(list_ref_alt)
+                    tumor_base_binaries.append(0.0)
 
-            for nt in nts:
-                list_nt = []
-                for idx in range(50):
-                    base = list_normal_base[idx]
-                    if base == nt:
-                        list_nt.append(1.0)
-                    else:
-                        list_nt.append(0.0)
-                list_one_hot_encoding_array.append(list_nt)
+            encode_result.append(tumor_base_binaries)
 
-            list_array.append(list_one_hot_encoding_array)
+        # make rows for alternative alleles in tumor alleles
+        alt_allele_binaries = []
 
-        #list_array2 = numpy.zeros((50, 100, 9))
-        #list_array2 = numpy.zeros((50, 1000, 9))
-        list_array2 = numpy.zeros((50, self.width, 9))
-        for idx, data_point in enumerate(list_array):
-            for idx2, channel in enumerate(data_point):
-                for idx3, ele in enumerate(channel):
-                    list_array2[idx3][idx][idx2] += ele
+        for base in tumor_alleles:
+            if base == variant.ref:
+                alt_allele_binaries.append(0.0)
+            elif base == variant.alt:
+                alt_allele_binaries.append(1.0)
+            else:
+                alt_allele_binaries.append(0.5)  # very exceptional
 
-        dic_vaf = dict()
-        for x in range(101):
-            dic_vaf[round(0.01*x, 2)] = 0
+        encode_result.append(alt_allele_binaries)
 
-        for vaf in list_vaf:
-            dic_vaf[round(vaf, 2)] += 1
+        # make rows for types of normal alleles
+        for nt in nts:
+            normal_base_binaries = []
 
-        total_count = sum(dic_vaf.values())
-        for key in dic_vaf.keys():
-            dic_vaf[key] = dic_vaf[key]/total_count
+            for base in normal_alleles:
+                if base == nt:
+                    normal_base_binaries.append(1.0)
+                else:
+                    normal_base_binaries.append(0.0)
 
-        list_vaf2 = []
-        for vaf in sorted(dic_vaf.keys()):
-            list_vaf2.append((vaf, dic_vaf[vaf]))
+            encode_result.append(normal_base_binaries)
 
-        #print(list_vaf2)
-        #print(sum([x[1] for x in list_vaf2]))
+        return encode_result
 
-        list_vaf_image = []
-        for vaf in list_vaf2:
-            col = []
-            #for idx in range(100-int(vaf[0]*100)):
-            zero_idx = max(0, self.hist_height-int(vaf[1]*1000))
-            for idx in range(zero_idx):
-                col.append(0.0)
-            #for idx in range(int(vaf[0]*100)):
-            one_idx = min(self.hist_height, int(vaf[1]*1000))
-            for idx in range(one_idx):
-                col.append(1.0)
-            list_vaf_image.append(col)
 
-        list_vaf_image = numpy.array(list_vaf_image).T
-        list_vaf_image = list_vaf_image[:,:,numpy.newaxis]
-        #list_vaf3 = sorted(list_vaf2, key=lambda x:x[1], reverse=True)
+class _Variant:
+    def __init__(self):
+        self.chrom = '.'
+        self.pos = 0  # 0-based
+        self.ref = '.'
+        self.alt = '.'
+        self.lodt = 0.0
+        self.vaf = 0.0
 
-        dic_array = {'read_image':list_array2, 'vaf_hist_image':list_vaf_image}
+    def __str__(self):
+        return f'{self.chrom}\t{self.pos}\t{self.ref}\t{self.alt}\t{self.lodt}\t{self.vaf}'
 
-        return dic_array
+    def parse_tsv_entry(self, tsv_entry):
+        # TSV cols: chrom, pos, ref, alt, LODt score, VAF
+        tsv_fields = tsv_entry.strip().split('\t')
+        self.chrom = tsv_fields[0]
+        self.pos = int(tsv_fields[1])
+        self.ref = tsv_fields[2]
+        self.alt = tsv_fields[3]
+        self.lodt = float(tsv_fields[4])
+        self.vaf = float(tsv_fields[5])
 
-print(time.ctime())
-MakeCNNImage()
-print(time.ctime())
+    @staticmethod
+    def parse_tsv_file(tsv_file_path):
+        variants = []
+
+        with open(tsv_file_path, 'r') as tsv_file:
+            tsv_file.readline()  # remove a header
+
+            for tsv_entry in tsv_file:
+                variant = _Variant()
+                variant.parse_tsv_entry(tsv_entry)
+                variants.append(variant)
+
+        return variants
+
+
+if __name__ == '__main__':
+    print(time.ctime(), file=sys.stderr)
+    ImageMaker()
+    print(time.ctime(), file=sys.stderr)
