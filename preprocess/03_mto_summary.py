76c95ca7b71653f5abe04d4e71c390b9c804876c
Minwoo Jeong <jmw95217@gmail.com>
2019-07-17 12:49:12 +0900

    add more information in the mto summary files

--- preprocess/03_mto_summary.py
+++ preprocess/03_mto_summary.py
@@ -24,9 +24,18 @@ class _Variant:
         self.alt = '.'
         self.lodt = 0.0
         self.vaf = 0.0
+        self.t_ref_count = 0
+        self.t_alt_count = 0
+        self.t_alt_freq = 0.0
+        self.n_ref_count = 0
+        self.n_alt_count = 0
+        self.n_alt_freq = 0.0
+        self.judge = ''
 
     def __str__(self):
-        return f'{self.chrom}\t{self.pos}\t{self.ref}\t{self.alt}\t{self.lodt}\t{self.vaf}'
+        return f'{self.chrom}\t{self.pos}\t{self.ref}\t{self.alt}\t{self.lodt}\t{self.vaf}\t' \
+               f'{self.t_ref_count}\t{self.t_alt_count}\t{self.t_alt_freq}\t' \
+               f'{self.n_ref_count}\t{self.n_alt_count}\t{self.n_alt_freq}\t{self.judge}'
 
     @staticmethod
     def parse_mto_file(mto_path):
@@ -49,19 +58,33 @@ class _Variant:
             vaf_idx = header_fields.index('tumor_f')
             judge_idx = header_fields.index('judgement')
 
+            t_ref_count_idx = header_fields.index('t_ref_count')
+            t_alt_count_idx = header_fields.index('t_alt_count')
+            n_ref_count_idx = header_fields.index('n_ref_count')
+            n_alt_count_idx = header_fields.index('n_alt_count')
+
             for mto_entry in mto_file:
-                fields = mto_entry.strip().split('\t')
-                chrom = fields[chrom_idx]
-                judge = fields[judge_idx]
+                cols = mto_entry.strip().split('\t')
+                chrom = cols[chrom_idx]
+                judge = cols[judge_idx]
 
                 if regex_chr.match(chrom) and judge == 'KEEP':
                     variant = _Variant()
                     variant.chrom = chrom
-                    variant.pos = int(fields[pos_idx])
-                    variant.ref = fields[ref_idx]
-                    variant.alt = fields[alt_idx]
-                    variant.lodt = float(fields[lodt_idx])
-                    variant.vaf = float(fields[vaf_idx])
+                    variant.pos = int(cols[pos_idx])
+                    variant.ref = cols[ref_idx]
+                    variant.alt = cols[alt_idx]
+                    variant.lodt = float(cols[lodt_idx])
+                    variant.vaf = float(cols[vaf_idx])
+
+                    variant.t_ref_count = int(cols[t_ref_count_idx])
+                    variant.t_alt_count = int(cols[t_alt_count_idx])
+                    variant.n_ref_count = int(cols[n_ref_count_idx])
+                    variant.n_alt_count = int(cols[n_alt_count_idx])
+
+                    variant.t_alt_freq = variant.t_alt_count / (variant.t_ref_count + variant.t_alt_count)
+                    variant.n_alt_freq = variant.n_alt_count / (variant.n_ref_count + variant.n_alt_count)
+                    variant.judge = judge
                     variants.append(variant)
 
         return variants
@@ -81,8 +104,8 @@ def main():
     log_dir = f'{PROJECT_DIR}/log/{job_name_prefix}/{time_stamp()}'
 
     # param settings
-    cells = ['HCC1143', 'HCC1187', 'HCC1954', 'HCC2218']
-    depths = ['10x', '20x', '30x', '40x', '50x']
+    cells = ['HCC1143', 'HCC1954']
+    depths = ['30x']
 
     norm_contams = [2.5, 5, 7.5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95]  # unit: percent
 
@@ -91,9 +114,9 @@ def main():
     for cell_line in cells:
         for depth in depths:
             # path settings
-            mto_dir = f'{PROJECT_DIR}/results/mutect-output/{cell_line}/{depth}'
+            mto_dir = f'{PROJECT_DIR}/results/mutect-output-depth-norm/{cell_line}/{depth}'
             mto_path_format = f'{mto_dir}/{cell_line}.%s.{depth}.mto'
-            mto_summary_dir = f'{PROJECT_DIR}/results/mto-summary/{cell_line}/{depth}'
+            mto_summary_dir = f'{PROJECT_DIR}/results/mto-summary-depth-norm/{cell_line}/{depth}'
             mto_summary_path_format = f'{mto_summary_dir}/{cell_line}.%s.{depth}.tsv'
             os.makedirs(mto_summary_dir, exist_ok=True)
 
@@ -130,7 +153,9 @@ def make_variant_summary(out_tsv_path, in_mto_path):
     """
     # parse mto
     variants = _Variant.parse_mto_file(in_mto_path)
-    tsv_header = 'chrom\tpos\tref_allele\talt_allele\tlod_t_score\tvaf'
+    tsv_header = 'chrom\tpos\tref_allele\talt_allele\tlod_t_score\tvaf\t' \
+                 't_ref_count\tt_alt_count\tt_alt_freq\t' \
+                 'n_ref_count\tn_alt_count\tn_alt_freq\tjudgement'
 
     with open(out_tsv_path, 'w') as out_tsv_file:
         print(tsv_header, file=out_tsv_file)
