55dd4ed2ac33627875fab2a966e4012a6f1fea0f
Minwoo Jeong <jmw95217@gmail.com>
2019-08-01 10:02:33 +0900

    use Pandas

--- src/training/05_sample_variants.py
+++ src/training/05_sample_variants.py
@@ -10,7 +10,7 @@ The TSV files will be used as data sets for learning
 from lab.job import Job, qsub_sge
 from lab.utils import time_stamp, eprint
 
-import numpy as np
+import pandas as pd
 import os
 import sys
 
@@ -38,19 +38,19 @@ def main():
     num_rand_variant = 10000  # No. randomly sampled variants
     num_sampling = 1000  # No. attempts of sampling
 
-    var_tsv_subdir_names = ['train-set', 'valid-set']
+    variant_classes = ['train-set', 'valid-set']
     cell_lines = ['HCC1143', 'HCC1954']
     depths = ['30x']
     norm_contams = [2.5, 5, 7.5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95]  # unit: percent
 
     jobs = []  # a list of the 'Job' class
 
-    for subdir_name in var_tsv_subdir_names:
+    for variant_class in variant_classes:
         for cell_line in cell_lines:
             for depth in depths:
                 # path settings
-                in_var_tsv_dir = f'{var_tsv_dir}/{subdir_name}/{cell_line}/{depth}'
-                out_var_tsv_dir = f'{var_tsv_dir}/{subdir_name}-samples/{cell_line}/{depth}'
+                in_var_tsv_dir = f'{var_tsv_dir}/{variant_class}/{cell_line}/{depth}'
+                out_var_tsv_dir = f'{var_tsv_dir}/{variant_class}-samples/{cell_line}/{depth}'
 
                 for norm_contam in norm_contams:
                     tumor_purity = 100 - norm_contam
@@ -89,7 +89,7 @@ def main():
         qsub_sge(jobs, queue, log_dir)
 
 
-def write_variant_sample(out_tsv_path, in_tsv_path, num_rand_var, num_top_lod_var=None):
+def write_variant_sample(out_tsv_path, in_tsv_path, num_rand_var, num_top_lodt_var=None):
     """
     Parse the input TSV file and randomly sample variants.
     From the variant set, get variants with top LODt score and store essential information of the variants as TSV file.
@@ -99,83 +99,35 @@ def write_variant_sample(out_tsv_path, in_tsv_path, num_rand_var, num_top_lod_va
     :param out_tsv_path: a path of an output TSV file
     :param in_tsv_path: a path of a input TSV file
     :param num_rand_var: No. of randomly sampled variants
-    :param num_top_lod_var: No. of variants with top LODt scores (optional)
+    :param num_top_lodt_var: No. of variants with top LODt scores (optional)
     """
     num_rand_var = int(num_rand_var)
 
-    if num_top_lod_var is None:
-        num_top_lod_var = num_rand_var
+    if num_top_lodt_var is None:
+        num_top_lodt_var = num_rand_var
     else:
-        num_top_lod_var = int(num_top_lod_var)
+        num_top_lodt_var = int(num_top_lodt_var)
+
+    assert num_rand_var >= num_top_lodt_var
 
     eprint('[LOG] Parameters')
     eprint(f'[LOG] --- Random sample size: {num_rand_var}')
-    eprint(f'[LOG] --- No. variants with top LODt scores: {num_top_lod_var}')
+    eprint(f'[LOG] --- No. variants with top LODt scores: {num_top_lodt_var}')
     eprint()
 
-    variants = []
-
-    with open(in_tsv_path, 'r') as in_tsv_file:
-        header = in_tsv_file.readline().strip()
-
-        for line in in_tsv_file:
-            variant = _Variant()
-            variant.parse_tsv_entry(line)
-            variants.append(variant)
-
-    var_cnt = len(variants)
+    variant_df = pd.read_table(in_tsv_path)
+    var_cnt = len(variant_df.index)
 
     eprint('[LOG] Random sampling')
     if var_cnt >= num_rand_var:
-        random_variants = list(np.random.choice(variants, num_rand_var, replace=False))
+        variant_df = variant_df.sample(n=num_rand_var, replace=False)
     else:
-        random_variants = list(np.random.choice(variants, num_rand_var, replace=True))
+        variant_df = variant_df.sample(n=num_rand_var, replace=True)
 
-    random_variants.sort(key=lambda x: x.lodt, reverse=True)  # sorted by their LODt score
-
-    eprint(f'[LOG] Write the information of variants with top {num_top_lod_var} LODt')
-    with open(out_tsv_path, 'w') as out_tsv_file:
-        print(header, file=out_tsv_file)
-
-        for i in range(num_top_lod_var):
-            print(random_variants[i], file=out_tsv_file)
-
-
-class _Variant:
-    def __init__(self):
-        self.chrom = '.'
-        self.pos = 0
-        self.ref = '.'
-        self.alt = '.'
-        self.lodt = 0.0
-        self.vaf = 0.0
-
-    def __str__(self):
-        return f'{self.chrom}\t{self.pos}\t{self.ref}\t{self.alt}\t{self.lodt}\t{self.vaf}'
-
-    def parse_tsv_entry(self, tsv_entry):
-        # TSV cols: chrom, pos, ref, alt, LODt score, VAF
-        tsv_fields = tsv_entry.strip().split('\t')
-        self.chrom = tsv_fields[0]
-        self.pos = int(tsv_fields[1])
-        self.ref = tsv_fields[2]
-        self.alt = tsv_fields[3]
-        self.lodt = float(tsv_fields[4])
-        self.vaf = float(tsv_fields[5])
-
-    @staticmethod
-    def parse_tsv_file(tsv_file_path):
-        variants = []
-
-        with open(tsv_file_path, 'r') as tsv_file:
-            tsv_file.readline()  # remove a header
-
-            for tsv_entry in tsv_file:
-                variant = _Variant()
-                variant.parse_tsv_entry(tsv_entry)
-                variants.append(variant)
-
-        return variants
+    variant_df = variant_df.sort_values(by='t_lod_fstar', ascending=False)
+    variant_df = variant_df.iloc[:num_top_lodt_var]
+    variant_df = variant_df.sort_index()
+    variant_df.to_csv(out_tsv_path, sep='\t', index=False)
 
 
 if __name__ == '__main__':
