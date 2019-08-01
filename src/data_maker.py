e03bae219611680380d9e90743fd7d12cf9a439f
Minwoo Jeong <jmw95217@gmail.com>
2019-08-01 10:42:00 +0900

    use VAF histogram data instead of previous images

---
+++ src/data_maker.py
@@ -0,0 +1,48 @@
+#!/extdata6/Doyeon/anaconda3/envs/deep-purity/bin/python3.6
+"""
+Make data for training or testing our deep learning model
+
+* Arguments of this script
+    1. out_data_path = a path of a VAF histogram (that will be stored using h5py)
+    2. var_tsv_file = a path of a mto summary file
+    3. tumor purity (optional; only used for learning): a ratio of tumor samples
+"""
+
+import numpy as np
+import pandas as pd
+import h5py
+import sys
+
+# argument parsing
+out_data_path = sys.argv[1]
+var_tsv_path = sys.argv[2]
+tumor_purity = float(sys.argv[3]) if len(sys.argv) >= 3 else None  # used for training
+
+# params
+top_lodt_frac = 0.1
+
+# make a vaf histogram
+print('[LOG] Make a VAF histogram')
+variant_df = pd.read_table(var_tsv_path)
+variant_df = variant_df.sort_values(by=['t_lod_fstar'], ascending=False).reset_index(drop=True)
+variant_cnt = int(len(variant_df.index) * top_lodt_frac)
+variant_df = variant_df[:variant_cnt]
+
+vaf_hist = np.array([0 for _ in range(101)])  # each item represents the number of variants for each VAF (unit: 0.01)
+
+for _, variant_row in variant_df.iterrows():
+    vaf = variant_row['tumor_f']
+    vaf_hist[int(round(vaf, 2) * 100)] += 1
+
+vaf_hist = vaf_hist / variant_cnt  # normalization
+
+# store the vaf histogram
+print('[LOG] Store the VAF histogram')
+learn_info_dict = {
+    'vaf_hist_array': vaf_hist,
+    'tumor_purity': tumor_purity
+}
+
+with h5py.File(out_data_path, 'w') as outfile:
+    outfile.create_dataset('vaf_hist_array', data=vaf_hist)
+    outfile.create_dataset('tumor_purity', data=tumor_purity)
--- src/training/06_make_learning_data.py
+++ src/training/06_make_learning_data.py
@@ -27,84 +27,42 @@ def main():
     log_dir = f'{PROJECT_DIR}/log/{job_name_prefix}/{time_stamp()}'
 
     # path settings
-    image_maker_script = f'{PROJECT_DIR}/src/image_maker.py'
-    image_set_dir = f'{PROJECT_DIR}/image-set-test-3'
-    os.makedirs(image_set_dir, exist_ok=True)
+    data_maker_script = f'{PROJECT_DIR}/src/data_maker.py'
+    var_tsv_dir = f'{PROJECT_DIR}/data/variants-tsv'
+    learn_data_dir = f'{PROJECT_DIR}/data/learning-data'
+    data_list_dir = f'{PROJECT_DIR}/data/learning-data-list'  # output
+    os.makedirs(data_list_dir, exist_ok=True)
 
     # param settings
-    hist_width = 1000
-    hist_height = 100
-    mode = 1  # 0: images of variants (from 03_summarize_mto.py), 1: images of variant samples (from 05_sample_variants.py)
-
-    # for mode 1
-    m = 1000  # No. randomly sampled variants
+    m = 10000  # No. randomly sampled variants
     num_iter = 1000  # No. attempts of sampling
 
+    data_classes = ['train-set', 'valid-set']
     cell_lines = ['HCC1143', 'HCC1954']
     depths = ['30x']
-    norm_contams = [2.5, 5, 7.5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95]  # unit: percent
+    norm_contams = [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95]  # unit: percent
 
     jobs = []  # a list of the 'Job' class
 
-    for cell_line in cell_lines:
-        for depth in depths:
-            # in-loop path settings
-            tumor_bam_dir = f'/extdata4/baeklab/minwoo/data/TCGA-HCC-MIX/{cell_line}/{depth}'
-            norm_bam_path = f'/extdata4/baeklab/minwoo/data/TCGA-HCC/{cell_line}/{cell_line}.NORMAL.{depth}.bam'
+    for data_class in data_classes:
+        data_list_path = f'{data_list_dir}/{data_class.replace("-", "_")}_data_paths.txt'
+        learn_data_paths = []
 
-            if mode == 0:  # make images of variants
-                image_set_path = f'{image_set_dir}/{cell_line}_{depth}.txt'
-                image_paths = []
-
-                var_tsv_dir = f'{PROJECT_DIR}/results/mto-summary/{cell_line}/{depth}'  # a result of 03_summarize_mto.py
-                out_image_dir = f'{PROJECT_DIR}/results/variant-images/{cell_line}/{depth}'
-                os.makedirs(out_image_dir, exist_ok=True)
-
-                for norm_contam in norm_contams:
-                    tumor_purity = 100 - norm_contam
-                    purity_tag = f'n{int(norm_contam)}t{int(tumor_purity)}'
-                    norm_contam_ratio = norm_contam / 100
-
-                    # in-loop path settings
-                    tumor_bam_path = f'{tumor_bam_dir}/{cell_line}.{purity_tag}.{depth}.bam'
-                    var_tsv_path = f'{var_tsv_dir}/{cell_line}.{purity_tag}.{depth}.tsv'
-
-                    if not os.path.isfile(var_tsv_path):
-                        continue
-
-                    var_image_path = f'{out_image_dir}/{cell_line}.{purity_tag}.{depth}.pkl'  # output
-                    image_paths.append(var_image_path)
-
-                    cmd = f'{image_maker_script} {in_tsv_path} {norm_contam_ratio} {tumor_bam_path} ' \
-                          f'{norm_bam_path} {var_image_path} {hist_width} {hist_height};'
-
-                    if is_test:
-                        print(cmd)
-                    else:
-                        prev_job_name = f'{prev_job_prefix}.{cell_line}.{depth}.{purity_tag}'
-                        one_job_name = f'{job_name_prefix}.{cell_line}.{depth}.{purity_tag}'
-                        one_job = Job(one_job_name, cmd, hold_jid=prev_job_name)
-                        jobs.append(one_job)
-
-            else:  # make images of random variants
-                image_set_path = f'{image_set_dir}/random_{m}_{cell_line}_{depth}.txt'
-                image_paths = []
-
+        for cell_line in cell_lines:
+            for depth in depths:
                 for norm_contam in norm_contams:
                     tumor_purity = 100 - norm_contam
                     purity_tag = f'n{int(norm_contam)}t{int(tumor_purity)}'
                     tumor_purity_ratio = tumor_purity / 100
 
                     # in-loop path settings
-                    tumor_bam_path = f'{tumor_bam_dir}/{cell_line}.{purity_tag}.{depth}.bam'
-                    var_sample_dir = f'{PROJECT_DIR}/results/variant-samples/{m}/{cell_line}/{depth}/{purity_tag}'
+                    var_sample_dir = f'{var_tsv_dir}/{data_class}-samples/{cell_line}/{depth}/{purity_tag}'
 
                     if not os.path.isdir(var_sample_dir):
                         continue
 
-                    out_image_dir = f'{PROJECT_DIR}/results/variant-samples-images-test/' \
-                                    f'{m}/{cell_line}/{depth}/{purity_tag}'
-                    os.makedirs(out_image_dir, exist_ok=True)
+                    output_dir = f'{learn_data_dir}/{data_class}/{cell_line}/{depth}/{purity_tag}'
+                    os.makedirs(output_dir, exist_ok=True)
 
                     cmd = ''
                     job_index = 1
@@ -112,28 +70,27 @@ def main():
 
                     for i in range(num_iter):
                         in_tsv_path = f'{var_sample_dir}/rand_{m}_{i+1:04}.tsv'
-                        out_image_path = f'{out_image_dir}/rand_{m}_{i+1:04}.pkl'
-                        image_paths.append(out_image_path)
-
-                        cmd += f'{image_maker_script} {out_image_path} {in_tsv_path} ' \
-                               f'{tumor_bam_path} {norm_bam_path} {tumor_purity_ratio};'
+                        out_vaf_hist_path = f'{output_dir}/rand_{m}_{i+1:04}.hdf5'
+                        learn_data_paths.append(out_vaf_hist_path)
+                        cmd += f'{data_maker_script} {out_vaf_hist_path} {in_tsv_path} {tumor_purity_ratio};'
 
                         if i % job_cnt_one_cmd == job_cnt_one_cmd - 1:
                             if is_test:
                                 print(cmd)
                             else:
-                                prev_job_name = f'{prev_job_prefix}.{cell_line}.{depth}.{purity_tag}'
-                                one_job_name = f'{job_name_prefix}.{cell_line}.{depth}.{purity_tag}.Random.{job_index}'
+                                prev_job_name = f'{prev_job_prefix}.{data_class}.{cell_line}.{depth}.{purity_tag}'
+                                one_job_name = f'{job_name_prefix}.{data_class}.{cell_line}.{depth}.{purity_tag}.' \
+                                               f'Random.{job_index}'
                                 one_job = Job(one_job_name, cmd, hold_jid=prev_job_name)
                                 jobs.append(one_job)
 
                             cmd = ''  # reset
                             job_index += 1
 
-            # make a list of images made by this script for training and testing the model
-            with open(image_set_path, 'w') as image_set_file:
-                for image_path in image_paths:
-                    print(image_path, file=image_set_file)
+        # make a list of vaf_hists made by this script for training and testing the model
+        with open(data_list_path, 'w') as vaf_hist_set_file:
+            for learn_data_path in learn_data_paths:
+                print(learn_data_path, file=vaf_hist_set_file)
 
     if not is_test:
         qsub_sge(jobs, queue, log_dir)
