1f39c435aa2224cb9b224a34de3b97570bb2bdc8
Minwoo Jeong <jmw95217@gmail.com>
2019-09-24 15:03:44 +0900

    add the platinum benchmark dataset as an option

--- src/training/01_in_silico_contam.py
+++ src/training/01_in_silico_contam.py
@@ -1,4 +1,4 @@
-#!/home/sonic/baeklab/Hyeonseong/anaconda3/envs/minwoo/bin/python
+#!/extdata6/Doyeon/anaconda3/envs/deep-purity/bin/python3.6
 """
 This script is used to mix normal reads (a%) and tumor reads ((1 - a)%) then make virtual tumor data in silico.
 (a: normal contamination ratio)
@@ -26,29 +26,34 @@ def main():
     log_dir = f'{PROJECT_DIR}/log/{job_name_prefix}/{time_stamp()}'
 
     # param settings
-    benchmark_list = ['tcga-benchmark4', 'giab']
+    benchmark_list = ['tcga-benchmark4', 'giab', 'platinum']
     cell_line_dict = {
         'tcga-benchmark4': ['HCC1143', 'HCC1954', 'HCC1187', 'HCC2218'],
-        'giab': ['HG003_NA24149_father', 'HG004_NA24143_mother'],
+        'giab': ['HG002-HG003', 'HG002-HG004'],
+        'platinum': ['NA12877-NA12889', 'NA12877-NA12890', 'NA12878-NA12891', 'NA12878-NA12892']
     }
     depth_dict = {
         'tcga-benchmark4': ['30x'],
         'giab': ['30x', '100x', '150x'],
+        'platinum': ['30x'],
     }
+
     benchmark = benchmark_list[1]
     cell_lines = cell_line_dict[benchmark]
     depths = depth_dict[benchmark]
+
+    # path settings
+    bam_dir_dict = {
+        'tcga-benchmark4': '/extdata4/baeklab/minwoo/data/TCGA-HCC-DEPTH-NORM',
+        'giab': '/extdata4/baeklab/minwoo/data/giab/illumina-paired-wes',
+        'platinum': '/extdata4/baeklab/minwoo/data/platinum',
+    }
 
     # script path settings
     samtools = '/home/lab/anaconda3/envs/Sukjun/bin/samtools'
     java = '/extdata6/Doyeon/anaconda3/envs/deep-purity/bin/java'
     picard = '/extdata6/Beomman/bins/picard/build/libs/picard.jar'
 
-    # path settings
-    bam_dir_dict = {
-        'tcga-benchmark4': '/extdata4/baeklab/minwoo/data/TCGA-HCC-DEPTH-NORM',
-        'giab': '/extdata4/baeklab/minwoo/data/giab/illumina-paired-wes'
-    }
     bam_dir = bam_dir_dict[benchmark]
     jobs = []  # a list of the 'Job' class
     norm_contams = list(range(5, 100, 5))
@@ -57,9 +62,28 @@ def main():
         for depth in depths:
             # in-loop path settings
             if benchmark == 'giab':
-                tumor_bam_path = f'{bam_dir}/{depth}/HG002_NA24385_son.bam'
-                norm_bam_path = f'{bam_dir}/{depth}/{cell_line}.bam'
-                out_bam_dir = f'/extdata4/baeklab/minwoo/data/giab-mix/HG002-{cell_line.split("_")[0]}/{depth}'
+                bam_path_table = {
+                    'HG002': f'{bam_dir}/{depth}/HG002_NA24835_son.bam',
+                    'HG003': f'{bam_dir}/{depth}/HG003_NA24149_father.bam',
+                    'HG004': f'{bam_dir}/{depth}/HG004_NA24143_mother.bam',
+                }
+                tumor_id, norm_id = cell_line.split('-')
+                tumor_bam_path = bam_path_table[tumor_id]
+                norm_bam_path = bam_path_table[norm_id]
+                out_bam_dir = f'/extdata4/baeklab/minwoo/data/giab-mix/{cell_line}/{depth}'
+            elif benchmark == 'platinum':
+                bam_path_table = {
+                    'NA12877': f'{bam_dir}/{depth}/NA12877.{depth}.bam',
+                    'NA12878': f'{bam_dir}/{depth}/NA12878.{depth}.bam',
+                    'NA12889': f'{bam_dir}/{depth}/NA12889.{depth}.bam',
+                    'NA12890': f'{bam_dir}/{depth}/NA12890.{depth}.bam',
+                    'NA12891': f'{bam_dir}/{depth}/NA12891.{depth}.bam',
+                    'NA12892': f'{bam_dir}/{depth}/NA12892.{depth}.bam',
+                }
+                tumor_id, norm_id = cell_line.split('-')
+                tumor_bam_path = bam_path_table[tumor_id]
+                norm_bam_path = bam_path_table[norm_id]
+                out_bam_dir = f'/extdata4/baeklab/minwoo/data/platinum-mix/{cell_line}/{depth}'
             else:  # tcga-benchmark4
                 tumor_bam_path = f'{bam_dir}/{cell_line}/{cell_line}.TUMOR.{depth}.bam'
                 norm_bam_path = f'{bam_dir}/{cell_line}/{cell_line}.NORMAL.{depth}.bam'
@@ -75,12 +99,7 @@ def main():
 
                 # in-loop path settings
                 purity_tag = f'n{int(contam)}t{int(purity)}'
-
-                if benchmark == 'tcga-benchmark4':
-                    out_bam_path = f'{out_bam_dir}/{cell_line}.{purity_tag}.{depth}.bam'
-                else:  # GIAB
-                    out_bam_path = f'{out_bam_dir}/HG002-{cell_line.split("_")[0]}.{purity_tag}.{depth}.bam'
-
+                out_bam_path = f'{out_bam_dir}/{cell_line}.{purity_tag}.{depth}.bam'
                 tumor_temp_bam_path = f'{temp_dir}/{os.path.basename(tumor_bam_path)}'
                 tumor_temp_bam_path = tumor_temp_bam_path.replace('.bam', '.%d%%.bam' % purity)
                 norm_temp_bam_path = f'{temp_dir}/{os.path.basename(norm_bam_path)}'
